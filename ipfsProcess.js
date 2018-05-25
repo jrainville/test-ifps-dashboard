d18065779f6d5d963ac76e53beeae32f5c478dc4
Iuri Matias <iuri.matias@gmail.com>
2018-05-25 17:44:10 -0400

    remove commented code

--- ipfsProcess.js
+++ ipfsProcess.js
@@ -1,6 +1,3 @@
-// const fs = require('../../core/fs');
-// const utils = require('../../utils/utils');
-// const ProcessLauncher = require('../../process/processLauncher');
 const child_process = require('child_process');
 const path = require('path');
 
@@ -14,37 +11,6 @@ class StorageProcessesLauncher {
 
     this.process = require('child_process').fork(path.join(__dirname, '/run.js'));
     console.dir(this.process);
-    //this.process = child_process.exec('ipfs daemon', {silent: true}, (err, _stdout, _stderr) => {
-    //  if (err) {
-    //    console.error(err);
-    //  }
-    //});
-    //this.process.stdout.on('data', (data) => {
-    //  if (!self.readyCalled && data.indexOf('Daemon is ready') > -1) {
-    //    self.readyCalled = true;
-    //    console.log(`IPFS process started`);
-    //    callback();
-    //  }
-    //  // console.log('IPFS: ' + data);
-    //});
-    //this.process.stderr.on('data', (data) => {
-    //  if (!self.readyCalled && data.indexOf('Daemon is ready') > -1) {
-    //    self.readyCalled = true;
-    //    console.log(`IPFS process started`);
-    //    callback();
-    //  }
-    //  // console.log('IPFS stderr: ' + data);
-    //});
-    //this.process.on('exit', (code) => {
-    //  if (code) {
-    //    console.error('IPFS exited with error code ' + code);
-    //  }
-    //});
-
-    //process.on('exit', () => {
-    //  console.log('we dead');
-    //  //this.process.kill();
-    //});
   }
 
   killItWithFire() {
